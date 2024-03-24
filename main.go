61d8e43b909c88282d4f072bbf7b92347723cd49
pulkit-singhall <singhal.pulkit667@gmail.com>
2024-03-24 22:10:39 +0530

    initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	godotenv.Load(".env")
+	PORT:=os.Getenv("PORT")
+	router := gin.New()
+
+	err:=router.Run(":"+PORT)
+	if err!=nil{
+		panic(err)
+	}
+}
